f92c36278fcadf46124d2b414051db2bf27d2665
adtoan-ai <adtoan2k4@gmail.com>
2026-07-10 09:47:42 +0700

    Add project files.

---
+++ FormSubmit/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ex4.Models; 
+
+namespace Ex4.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Username == "admin" && model.Password == "123")
+            {
+                ViewBag.Message = "Login Success"; 
+                ViewBag.Status = "success";       
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed";  
+                ViewBag.Status = "danger";        
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FormSubmit/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ex4.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
